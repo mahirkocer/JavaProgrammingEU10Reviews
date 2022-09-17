81b60292f06780b99a27d18c5359c47afe462008
AdamB25 <barisaltunit@gmail.com>
2022-09-17 13:01:48 -0400

    Week 2 DivideTwoNumber is done

---
+++ src/week02_09_17_2022/DivideTwoNumber.java
@@ -0,0 +1,35 @@
+package week02_09_17_2022;
+
+public class DivideTwoNumber {
+
+    public static void main(String[] args) {
+
+        //declare the first var
+        int firstNumber = 20;
+
+
+        //declare the second var
+       int secondNumber=3;
+
+        //calculate the result
+
+        double result=(double) firstNumber/secondNumber; // 6.666666666
+        //print
+
+        System.out.println("result = " + result);
+
+
+        double a=20;
+
+        double b=3;
+
+
+        int division=(int) (a/b);
+
+        System.out.println("division = " + division);
+
+
+    }
+
+
+}
