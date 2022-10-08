17cfb060d4bf41dfd53df4aeb6c3d1a1722d6871
AdamB25 <barsialtunit@gmail.com>
2022-10-08 15:02:20 -0400

    week 5

--- src/week05_10_08_2022/StringMethods.java
+++ src/week05_10_08_2022/StringMethods.java
@@ -56,12 +56,60 @@ public class StringMethods {
         System.out.println(sentence4.replaceFirst("java", "c#"));
 
         //if we want to get some part of the string we need to use substring()
-        //01234
+                     //01234
         String subs = "Cydeo";
         // get the yde from the subs variable
         System.out.println(subs.substring(1, 4));
         // get the Cy from he subs
         System.out.println(subs.substring(0, 2));
+        // get the "deo" from subs variable
+        System.out.println(subs.substring(2));
+
+        // det the "ydeo" from subs variable
+
+        System.out.println(subs.substring(1));
+
+        // if we want to multply(repeat) one word we need to use repeat()
+        String str4="Adam";
+        System.out.println(str4.repeat(4));
+
+        // if we want to check our string is empty or not we need to use isEmpty() method
+        String emptyWord=""; // isEmpty method is checking length
+        System.out.println(emptyWord.isEmpty());
+
+        // if we want to check our string is blank or not we need to use isBlank() method
+       String blankWord="   "; // it is checking spaces
+
+        System.out.println(blankWord.isBlank());
+
+        // if we want to compare two string values with exact matching we can use  equals()
+
+
+        // if we want to compare two string values without exact matching (lower case or upper case) we can use
+        // equalsIgnoreCase()
+
+        String str6="Word";
+        String str7="word";
+
+        System.out.println(str6.equals(str7)); // false
+        System.out.println(str6.equalsIgnoreCase(str7)); //true
+
+       // startsWith()  method is using for checking the string is starting with given char sequence or not
+        // endsWith()  method is using for checking the string is ending with given char sequence or not
+        String sentence5= "Java is a good language";
+        String word="Java";
+        System.out.println(sentence5.startsWith(word));
+
+        System.out.println(sentence5.endsWith("language"));
+
+        // contains method is using for checking the given char sequence is in string or not
+        String sentence6="Java is Java";
+
+
+
+
+
+
 
     }
 }
