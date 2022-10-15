71db6ae5d5eba57f44bb11db6b522db65b1adc85
AdamB25 <barsialtunit@gmail.com>
2022-10-15 11:29:17 -0400

    week 6

---
+++ src/week06_10_15_2022/T2_CountWords.java
@@ -0,0 +1,25 @@
+package week06_10_15_2022;
+
+public class T2_CountWords {
+
+    /*
+    Task 2: Given a sentence which is string and count the words in it.
+
+input:
+
+  String str="Java is java in everywhere is Java";
+
+  output:7
+     */
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+    }
+}
