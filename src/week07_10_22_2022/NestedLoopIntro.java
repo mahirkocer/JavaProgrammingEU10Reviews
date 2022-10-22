59781e16ac8298dbafe844f8c2481fc09da31911
AdamB25 <barsialtunit@gmail.com>
2022-10-22 10:50:37 -0400

    week 7

---
+++ src/week07_10_22_2022/NestedLoopIntro.java
@@ -0,0 +1,41 @@
+package week07_10_22_2022;
+
+public class NestedLoopIntro {
+
+    public static void main(String[] args) {
+
+       /*
+
+       *******
+       *******
+       *******
+       *******
+
+        */
+
+        int starNumberColumn=7;
+        int starNumberRow=4;
+
+        for (int i = 1; i <=starNumberRow ; i++) { //row number
+           // System.out.println();
+            for (int j = 1; j <=starNumberColumn ; j++) { //column number
+                System.out.print("*"); // 7 times *
+            }
+            System.out.println();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
