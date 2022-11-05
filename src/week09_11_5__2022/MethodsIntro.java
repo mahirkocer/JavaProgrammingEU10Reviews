0e261983859e23f9e97cd3f2d3a0bd37ad464f12
AdamB25 <barsialtunit@gmail.com>
2022-11-05 11:30:09 -0400

    week 9

--- src/week09_11_5__2022/MethodsIntro.java
+++ src/week09_11_5__2022/MethodsIntro.java
@@ -1,23 +1,42 @@
 package week09_11_5__2022;
 
+
+
 public class MethodsIntro {
 
     public static void main(String[] args) {
         // printHello5Times
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
+        printHello5Times();
         // printHello5Times
-       printHello5Times();
+        printHello5Times();
         // printHello5Times
-       printHello5Times();
+        printHello5Times();
 
+        //we want to add two number
+
+        int result = sum(5, 6); //11
+        //create method quick
+        //for windows alt+enter
+        //for mac option+enter
+
+               //multiply that number with 2 and show in console
+        multiplywith2AndPrint(result);
+
+        System.out.println(sum(20, 30));
 
 
     }
-    public static void printHello5Times(){
+
+    public static void multiplywith2AndPrint(int result) {
+        System.out.println(result*2);
+    }
+
+    public static int sum(int number1, int number2 ) {
+      return   number2+number1;
+    }
+
+
+    public static void printHello5Times() {
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
@@ -25,8 +44,6 @@ public class MethodsIntro {
         System.out.println("Hello");
 
     }
-
-
 
 
 }
