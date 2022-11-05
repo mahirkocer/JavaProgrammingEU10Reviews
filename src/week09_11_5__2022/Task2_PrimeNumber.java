9427bd4728e929e79abf0241f1ce1293d64f8a96
AdamB25 <barsialtunit@gmail.com>
2022-11-05 12:01:44 -0400

    week 9 Task 2

---
+++ src/week09_11_5__2022/Task2_PrimeNumber.java
@@ -0,0 +1,41 @@
+package week09_11_5__2022;
+
+public class Task2_PrimeNumber {
+    public static void main(String[] args) {
+        /*
+        Task 2 :   Write a method that can check if a number is prime or not
+
+				Note : Prime numbers are special numbers, greater than 1, that have exactly two factors, themselves and 1
+
+				7
+
+			is divisible by	 2
+			""	             3
+			""	             4
+			""	             5
+	        ""	             6
+
+         */
+
+        boolean result = isPrime(-8); //true
+        System.out.println(result);
+
+    }
+
+    public static boolean isPrime(int number) {
+        boolean isPrimeNumber = true;
+
+        if (number<2){
+            return false;
+        }
+
+        for (int i = 2; i < number; i++) {
+
+            if (number % i == 0) {
+                isPrimeNumber = false;
+                break;
+            }
+        }
+        return isPrimeNumber;
+    }
+}
