7d60a6ac9b82aab813933933fe5d292e3022dab3
AdamB25 <barsialtunit@gmail.com>
2022-11-05 10:52:17 -0400

    week 9

---
+++ src/week09_11_5__2022/MethodsIntro.java
@@ -0,0 +1,32 @@
+package week09_11_5__2022;
+
+public class MethodsIntro {
+
+    public static void main(String[] args) {
+        // printHello5Times
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        // printHello5Times
+       printHello5Times();
+        // printHello5Times
+       printHello5Times();
+
+
+
+    }
+    public static void printHello5Times(){
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
+    }
+
+
+
+
+}
