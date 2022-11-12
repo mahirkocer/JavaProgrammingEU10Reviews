62be4c0af1020b634435e2867727b9b04a30ed52
AdamB25 <barsialtunit@gmail.com>
2022-11-12 20:48:22 +0300

    week 10 Task 2

--- src/week10_11_12_2022/Task2_FourOrLess.java
+++ src/week10_11_12_2022/Task2_FourOrLess.java
@@ -1,5 +1,7 @@
 package week10_11_12_2022;
 
+import java.util.Arrays;
+
 public class Task2_FourOrLess {
 
     /*
@@ -13,7 +15,7 @@ public class Task2_FourOrLess {
 					Ex:
 
 					Input:
-					“apples”, “tree”, “loop, “cat”, “animal”, “shortcut”
+					"apples", "tree", "loop, "cat", "animal", "shortcut"
 
 					Output:
 					[ tree, loop, cat ]
@@ -22,10 +24,44 @@ public class Task2_FourOrLess {
 
     public static void main(String[] args) {
 
+        String[] words = {"apples", "tree", "loop", "cat", "animal", "shortcut" };
 
-
-
-
+        String[] result = fourOrLess(words);
+        System.out.println(Arrays.toString(result));
 
     }
+
+    public static String[] fourOrLess(String[] words) {
+        int number = findTheNumberOfWordLessThanFour(words);
+
+        /*
+         int counter = 0;
+        for (String word : words) {
+            if (word.length() <= 4)
+                counter++;
+        }
+        //return counter;
+    }
+
+         */
+        String[] array = new String[number];
+        int i = 0;
+        for (String word : words) {
+
+            if (word.length() <= 4)
+                array[i++] = word;
+        }
+
+        return array;
+    }
+
+    public static int findTheNumberOfWordLessThanFour(String[] words) {
+        int counter = 0;
+        for (String word : words) {
+            if (word.length() <= 4)
+                counter++;
+        }
+        return counter;
+    }
 }
+
