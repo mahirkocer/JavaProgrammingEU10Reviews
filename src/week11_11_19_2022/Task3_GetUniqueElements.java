1e05b278c67c26f82b2038c9bba4d001a31b869b
AdamB25 <barsialtunit@gmail.com>
2022-11-19 22:37:48 +0300

    week 11 Task 3 is ready

---
+++ src/week11_11_19_2022/Task3_GetUniqueElements.java
@@ -0,0 +1,23 @@
+package week11_11_19_2022;
+
+public class Task3_GetUniqueElements {
+/*
+Task 3 :
+
+Create a method that get two arrays as a parameters then return the arraylist which has contains unique elements in it.
+
+first array :1,2,3,4,5
+
+second array :4,5,6
+
+output:
+
+1,2,3,4,5,6
+ */
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
