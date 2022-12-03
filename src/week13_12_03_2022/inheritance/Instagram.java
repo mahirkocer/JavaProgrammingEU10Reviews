23665147034bfa1a95983217b04dec0e16ed0ed9
AdamB25 <barsialtunit@gmail.com>
2022-12-03 23:10:31 +0300

    week 13

---
+++ src/week13_12_03_2022/inheritance/Instagram.java
@@ -0,0 +1,46 @@
+package week13_12_03_2022.inheritance;
+
+public class Instagram extends MobileApp {
+
+
+    public Instagram(double version){
+        super("Instagram",version);
+    }
+
+    /*
+    public  Instagram(){
+       super();
+    }
+
+
+    */
+
+
+
+    /*
+    instead of that one I need to use extends keyword and MobileApp
+  public   String name;
+  public  double version;
+
+    public void useTheApp(int minutes){
+        System.out.println("The user is using " + name + " application for " + minutes + " minutes");
+
+    }
+
+
+    public  void  download(){
+        System.out.println(name + " application is downloading by the user.");
+    }
+
+
+    public String toString() {
+        return "MobileApp{" +
+                "name='" + name + '\'' +
+                ", version=" + version +
+                '}';
+    }
+
+    */
+
+
+}
---
+++ src/week13_12_03_2022/inheritance/Test.java
@@ -0,0 +1,20 @@
+package week13_12_03_2022.inheritance;
+
+public class Test {
+    public static void main(String[] args) {
+
+        String varia="Adam";
+        System.out.println(varia);
+        System.out.println(varia);
+        System.out.println(varia);
+        System.out.println(varia);
+        System.out.println(varia);
+        System.out.println(varia);
+        System.out.println(varia);
+        System.out.println(varia);
+
+
+
+
+    }
+}
