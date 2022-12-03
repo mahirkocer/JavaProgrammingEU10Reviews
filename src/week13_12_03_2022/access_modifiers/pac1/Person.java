a7f14509dd7ef404c914e195297e565e9a56696e
AdamB25 <barsialtunit@gmail.com>
2022-12-03 20:32:02 +0300

    week 13

---
+++ src/week13_12_03_2022/access_modifiers/pac1/Person.java
@@ -0,0 +1,16 @@
+package week13_12_03_2022.access_modifiers.pac1;
+
+public class Person {
+
+   public String name;
+   String lastName;
+   private int id;
+  static protected int age;
+
+
+   public void getid() {
+      System.out.println(id);
+   }
+
+
+}
