66c746eb505f03c3df0c0ac1aaf251552b8fd65e
AdamB25 <barsialtunit@gmail.com>
2022-12-03 19:55:42 +0300

    week 13

--- src/week13_12_03_2022/warmup/School.java
+++ src/week13_12_03_2022/warmup/School.java
@@ -22,7 +22,7 @@ public class School {
     static ArrayList<Teacher> teachers = new ArrayList<>();
 
 
-    static {
+  static  {
 
         Teacher teacher1 = new Teacher("Muhtar", "Abc", 'M', LocalDate.of(1987, 1, 1), 1L);
         Teacher teacher2 = new Teacher("Gurhan", "Abc", 'M', LocalDate.of(1977, 1, 1), 2L);
@@ -42,6 +42,28 @@ public class School {
         ArrayList<Teacher> result = getTeachersNameStartWith("M");
 
         System.out.println(result);
+
+        ArrayList<Teacher> females = getTeachersByGender('F');
+        System.out.println(females);
+    }
+
+    private static ArrayList<Teacher> getTeachersByGender(char gender) {
+        ArrayList<Teacher> result = new ArrayList<>();
+
+        //need to get teachers one by one
+        for (Teacher teacher : teachers) {
+
+            //check the gender if it is female
+            if (teacher.gender == gender)
+
+                //put in result
+                result.add(teacher);
+
+        }
+
+        return result;
+
+
     }
 
     private static ArrayList<Teacher> getTeachersNameStartWith(String prefix) {
@@ -49,7 +71,7 @@ public class School {
         ArrayList<Teacher> result = new ArrayList<>();
 
 
-             //I need to get teacher one by one
+        //I need to get teacher one by one
         for (Teacher teacher : teachers) {
 
             // I need to check the name if the name is starting with "M"
